02327781651c9804ea57581fa4841915541c8089
hyunsang-ahn <happylovetkd@naver.com>
2022-07-14 07:03:11 +0900

    movies list append

--- src/App.js
+++ src/App.js
@@ -5,9 +5,16 @@ function App() {
   return (
     <div className="App">
       <h1>let go build netflix</h1>
-      <Row title="넷플릭스 오리자널" fetchUrl={requests.fetchNetflixOriginals}/>
+      <Row title="넷플릭스 오리자널" fetchUrl={requests.fetchNetflixOriginals} isLargeRow={true}/>
       <Row title="현재 트랜드" fetchUrl={requests.fetchTrending}/>
-      
+      <Row title="최고 인기" fetchUrl={requests.fetchTopRated}/>
+      <Row title="액션 영화" fetchUrl={requests.fetchActionMovies}/>
+      <Row title="코메디 영화" fetchUrl={requests.fetchComedyMovies}/>
+      <Row title="호러 영화" fetchUrl={requests.fetchHorrorMovies}/>
+      <Row title="로맨스 영화" fetchUrl={requests.fetchRomanceMovies}/>
+
+      <Row title="다큐멘터리 영화" fetchUrl={requests.fetchDocumantaries}/>
+
     </div>
   );
 }
--- src/components/Row.js
+++ src/components/Row.js
@@ -1,24 +1,38 @@
 import React, { useState, useEffect } from 'react';
 import axios from './axios'
-
-const Row = ({ title, fetchUrl }) => {
+import './Row.css'
+const base_url = 'https://image.tmdb.org/t/p/original/'
+const Row = ({ title, fetchUrl, isLargeRow }) => {
   const [movies, setMovies] = useState([])
   useEffect(() => {
     // 만일 [] 라면 row가 로드될때 단 한번만 동작하고 다시 동작하지 않는다!
     // 만일 [movies] 라면 movies가 변할때마다 동작한다 즉, component did update의 역할을 한다.
-    async function fetchData(){
+    async function fetchData() {
       const requeset = await axios.get(fetchUrl)
       // https://api.themoviedb.org/3/discover/tv?api_key=${API_KEY}&with_networks=213"
-      console.log(requeset.data)
+      // console.log(requeset.data)
+      setMovies(requeset.data.results)
       return requeset
     }
     fetchData()
   }, [])
   return (
-    <div>
+    <div className='row'>
       {/* TITLE */}
       <h2>{title}</h2>
       {/* container -> posters */}
+      <div className="row_posters">
+        {movies.map((movie, i) => {
+          console.log('movie....', movie)
+          return (
+            <img
+              key={movie.id}
+              className={`row_poster ${isLargeRow && "row_posterLarge"}`}
+              src={`${base_url}${isLargeRow ? movie.poster_path : movie.backdrop_path
+                }`} alt={movie.name} />
+          )
+        })}
+      </div>
 
     </div>
   );
